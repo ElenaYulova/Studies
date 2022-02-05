2fdf073e87c34324ccca2658f0d67b050edc3a1e
Kissloroid <kissloroid@gmail.com>
2022-02-05 12:09:10 +0300

    Day 2 Homework. Part 1

---
+++ Lesson2.py
@@ -0,0 +1,73 @@
+# str_1 = ''
+# str_2 = ""
+# str_f = f''
+# str_3 = str()
+# str_4 = """
+# Text
+# is
+# different
+# lines
+# """
+#
+# str_ = ("very 'long'")
+# str_5 = (r"very \'long\'")
+# print(str_4)
+# print(str_5)
+# print(str_)
+# hello_str = 'hello' + ' ' + "world"
+# print(hello_str)
+# hello = "hello"
+# space = " "
+# world = "world"
+# print(id(hello))
+# print(id(space))
+# print(id(world))
+# hello_str = hello + space + world
+# print(hello_str)
+# print(id(hello_str))
+#
+# a = [1, 2, 3]
+# b = [1, 2, 3]
+#
+# print(a == b)
+# print(a is b)
+# print('id a = ', id(a))
+# print('id b = ', id(b))
+# c = 1000000000
+# d = 1000000000
+# print(c == d)
+# print(c is d)
+#
+# if a is None:  # предпочтительный вариант проверки
+#     pass
+# if a:      # Пустой список к примеру может быть допустимым вариантом ответа
+#     pass
+#
+
+# str_1 = 'abcdef'.lower()
+# str_2 = 'Abcdef'.lower()
+# print(str_1[1:4:1])
+# str_1 = str_1[:3] + 'z' + str_1[3:]
+# print(str_1)
+# print(ord(str_1[1])) # возвращает код
+# print(chr(63))       # возвращает элемент по коду
+# print(str_1.upper())
+# print(str_1.lower())
+# print(str_1.capitalize())
+# print(str_1 == str_2)
+# a = 'London is the capital'
+# # print(a.split())
+# a = ['London', 'is', 'the', 'capital']
+# print("\t".join(a))
+# a = "format_1"
+# b = "format_2"
+# c = "format_3"
+# template = f"template for {a} {2 + 5} {c.upper()}"
+# print(template)
+#string = 'abcd abcd abcd'
+# for char in string:
+#     if char != ' ':
+#         print(char)
+
+#
+
